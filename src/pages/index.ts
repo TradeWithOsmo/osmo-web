b7df7151371efaff1f9b9ed06023904a62464990
Fajar Arrizki <fajararrizki15@gmail.com>
2026-01-08 12:23:36 +0700

    add lederboard + Strategies

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -6,3 +6,4 @@ export { default as Portfolio } from './Portfolio';
 
 export { default as Points } from './Points';
 export { default as Leaderboard } from './Leaderboard';
+export { default as Strategies } from './Strategies';
